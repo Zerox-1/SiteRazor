9e9f2643bf94eea6ad493f624668f17ead7e9e24
Nail Husainov <1nprogh1@gmail.com>
2024-05-15 23:49:41 +0300

    Забыл добавить файл

--- Site/Models/ApplicationUser.cs
+++ Site/Models/ApplicationUser.cs
@@ -2,9 +2,12 @@
 {
     public class ApplicationUser
     {
+        public int Id { get; set; }
         public string Name { get; set; }
-        public string Address { get; set; }
-        public string City { get; set; }
-        public string PostalCode { get; set; }
+        public string SecondName { get; set; }
+        public string Email { get; set; }
+        public string Number { get; set; }
+        public string Password { get; set; }
+        public int Role { get; set; }
     }
 }
