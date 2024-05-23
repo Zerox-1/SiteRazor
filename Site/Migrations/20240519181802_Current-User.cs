1f0943aa391464768be4a82b451ac79b2d6524b9
Nail Husainov <1nprogh1@gmail.com>
2024-05-23 23:36:19 +0300

    обновление

--- Site/Migrations/20240519181802_Current-User.cs
+++ Site/Migrations/20240519181802_Current-User.cs
@@ -14,8 +14,7 @@ namespace Site.Migrations
                 name: "CurrentUser",
                 columns: table => new
                 {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Id = table.Column<int>(type: "int", nullable: false),
                     FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
                     LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
                     Phone = table.Column<string>(type: "nvarchar(max)", nullable: false),
---
+++ Site/Migrations/20240523203034_Rented.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Site.Migrations
+{
+    /// <inheritdoc />
+    public partial class Rented : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Rented",
+                table: "Room",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rented",
+                table: "Room");
+        }
+    }
+}
